33af7edfedd9f15e28631504c36a32b3301063c8
NadiaLayraAziza <nadialayra15@gmail.com>
2021-04-15 20:12:08 +0700

    Tugas 1 - Mengedit view edit.blade.php untuk menambahkan form edit image

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -20,7 +20,7 @@
                         </ul>
                     </div>
                 @endif
-                <form method="post" action="{{ route('mahasiswa.update', $Mahasiswa->Nim) }}" id="myForm">
+                <form method="post" action="{{ route('mahasiswa.update', $Mahasiswa->Nim) }}" id="myForm" enctype="multipart/form-data">
                 @csrf
                 @method('PUT') 
                     <div class="form-group">
@@ -30,6 +30,12 @@
                     <div class="form-group">
                         <label for="Nama">Nama</label> 
                         <input type="text" name="Nama" class="form-control" id="Nama" value="{{ $Mahasiswa->Nama }}" aria-describedby="Nama" > 
+                    </div>
+                    <div class="form-group">
+                        <label for="image">Foto</label>
+                        <input type="file" name="image" class="form-control" id="image" value="{{ $Mahasiswa->image }}" 
+                        required="required" aria-describedby="image" ></br>
+                        <img width="100px" height="100px" src="{{asset('storage/'.$Mahasiswa->image)}}">
                     </div>
                     <div class="form-group">
                         <label for="Tanggal_Lahir">Tanggal_Lahir</label> 
