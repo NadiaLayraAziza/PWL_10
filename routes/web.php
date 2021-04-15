e65bdad41993abdf1f12df399367d06848fe1c51
NadiaLayraAziza <nadialayra15@gmail.com>
2021-04-15 20:39:22 +0700

    Tugas 2 - Buat Route baru di routes/web.php untuk menjalankan fungsi cetak_khs

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::get('/article/cetak_pdf', [ArticleController::class, 'cetak_pdf']);
 Route::resource('mahasiswa', MahasiswaController::class);
 
 Route::get('mahasiswa/nilai/{Nim}', [MahasiswaController::class, 'nilai'])->name('mahasiswa.nilai');
+
+Route::get('/mahasiswa/cetak_khs/{Nim}', [MahasiswaController::class, 'cetak_khs'])->name('mahasiswa.pdf');
